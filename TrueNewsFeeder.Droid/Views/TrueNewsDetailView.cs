d29d45275c541926ff9292fc72a01a1723777e29
Temo Banos Gallardo <temoBGallardo@outlook.com>
2020-05-14 17:24:34 -0400

    Adding the details view

---
+++ TrueNewsFeeder.Droid/Views/TrueNewsDetailView.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.OS;
+using MvvmCross.Platforms.Android.Presenters.Attributes;
+
+namespace TrueNewsFeeder.Droid.Views
+{
+    [MvxActivityPresentation]
+    [Activity(Theme ="@style/AppTheme", MainLauncher = false)]
+    public class TrueNewsDetailView: Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState) 
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.true_news_detail);
+        }
+
+
+    }
+}
